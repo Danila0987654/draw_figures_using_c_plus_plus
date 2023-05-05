b1b090b40d611fe4c447f30b83cc432486241a1d
DanilaSokolov <sokolovdanila2000@gmail.com>
2023-05-05 07:27:08 +0300

    Refactored create_figures.cpp
    Optimized function create_rectangle, deleted unnecessary circles

--- functions/create_figures.cpp
+++ functions/create_figures.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 
 void create_rectangle(int input_column, int input_row) {
-    for (int column = 0; column < input_column; column++) {
-        std::cout << "*";
-    }
-    std::cout << "\n";
-
-    for (int row = 0; row < input_row -2; row++) {
-        std::cout << "*";
-
-        for (int column = 0; column < input_column - 2; column++) {
-            std::cout << " ";
+    for (int row = 0; row < input_row; row++) {
+        if (row == 0 || row == input_row - 1) {
+            for (int column = 0; column < input_column; column++) {
+                std::cout << "*";
+            }
+            std::cout << "\n";
         }
-        std::cout << "*\n";
-    }
 
-    for (int column = 0; column < input_column; column++) {
-        std::cout << "*";
+        else {
+            std::cout << "*";
+            for (int column = 0; column < input_column - 2; column++) {
+                std::cout << " ";
+            }
+            std::cout << "*\n";
+        }
+
     }
 }
 
 void create_circle(int length) {
     int radius = length * 2;
 
-    for (int column = 0; column < radius; column++) {
-        std::cout << "*";
-    }
-    std::cout << "\n";
     for (int row = 0; row < length; row++) {
         if (row == 0 || row == length - 1) {
             for (int column = 0; column < radius; column++) {
@@ -37,6 +33,7 @@ void create_circle(int length) {
             }
             std::cout << "\n";
         }
+
         else if (row == 1 || row == length - 2) {
             for (int column = 0; column < radius; column++) {
                 if ((column > 0 && column < 3) || (column > radius - 4 && column < radius - 1))
@@ -46,6 +43,7 @@ void create_circle(int length) {
             }
             std::cout << "\n";
         }
+
         else {
             for (int column = 0; column < radius; column++) {
                 if ((column >= 0 && column < 2) || (column > radius - 3 && column <= radius - 1))
